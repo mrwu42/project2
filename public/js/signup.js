5bbd5f5606458ce945c9222b86c66f178af59496
Tim-Shaffer <tcshaf24@ptd.net>
2020-04-04 21:00:38 -0400

    created initial signup and login process

---
+++ public/js/signup.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  // Getting references to our form and input
+  var signUpForm = $("form.signup");
+  var userInput = $("#userinput");
+  var passwordInput = $("input#password-input");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  signUpForm.on("submit", function(event) {
+    event.preventDefault();
+    var userData = {
+      username: userInput.val().trim(),
+      password: passwordInput.val().trim()
+    };
+
+    if (!userData.username || !userData.password) {
+      return;
+    }
+
+    // If we have an username and password, run the signUpUser function
+    signUpUser(userData.username, userData.password);
+    userInput.val("");
+    passwordInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  function signUpUser(username, password) {
+    $.post("/api/signup", {
+      username: username,
+      password: password
+    })
+      .then(function(data) {
+        window.location.replace("/members");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
