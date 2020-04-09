25e6dde978413346059721d72930f3344421ad37
Tim-Shaffer <tcshaf24@ptd.net>
2020-04-09 17:05:08 -0400

    updated progress bars for each attribute to load according to the DB values

--- public/js/members.js
+++ public/js/members.js
@@ -1,19 +1,25 @@
 // sample for the makeprogress bar
-var i = 0;
-
-function makeProgress() {
-  if (i < 100) {
+function makeProgress(attr, endpoint, i) {
+  var bar;
+  if (i < endpoint) {
     i = i + 1;
-    $("#hunger")
-      .css("width", i + "%")
-      .text(i + " %");
+    attr.css("width", i + "%").text(i + " %");
+    bar = setTimeout(makeProgress(attr, endpoint, i), 1000);
   }
   // Wait for sometime before running this script again
-  setTimeout("makeProgress()", 100);
+  // setTimeout("makeProgress()", 100);
+  clearTimeout(bar);
 }
 
 $(document).ready(function() {
-  makeProgress();
+  var dirty = $("#dirty");
+  var health = $("#health");
+  var hunger = $("#hunger");
+  var love = $("#love");
+  var play = $("#play");
+  var sleep = $("#sleep");
+
+  // makeProgress();
 
   // This does a GET request to figure out which user is logged in
   $.get("/api/user_data").then(function(data) {
@@ -38,14 +44,26 @@ $(document).ready(function() {
             CharacterId: charData.id
           };
           // create a new Pet for this user
-          $.post("/api/pet", petData).then(function(newPet){
+          $.post("/api/pet", petData).then(function(newPet) {
             console.log("Pet: " + JSON.stringify(newPet));
+            makeProgress(hunger, newPet.hungerProgress, 0);
+            makeProgress(play, newPet.playfulProgress, 0);
+            makeProgress(sleep, newPet.sleepProgress, 0);
+            makeProgress(love, newPet.wakeProgress, 0);
+            makeProgress(dirty, newPet.cleanProgress, 0);
+            makeProgress(health, newPet.medicationProgress, 0);
           });
         });
       } else {
         console.log("Existing Pet Info: " + JSON.stringify(resp));
         // $(".member-name").text(resp.User.username);
-      };
+        makeProgress(hunger, resp.hungerProgress, 0);
+        makeProgress(play, resp.playfulProgress, 0);
+        makeProgress(sleep, resp.sleepProgress, 0);
+        makeProgress(love, resp.wakeProgress, 0);
+        makeProgress(dirty, resp.cleanProgress, 0);
+        makeProgress(health, resp.medicationProgress, 0);
+      }
     });
   });
 });
